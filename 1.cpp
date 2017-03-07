5e1420b22889e9ae8ed52bfd51e66263c883b95a
Даниил Зазвонов < >
2017-03-07 19:37:53 +0700

    Perfom backround checks on specifiend

--- 1.cpp
+++ 1.cpp
@@ -44,6 +44,66 @@ int vich(int sum, int sr, int den)
     return den;
 }
 
+int proverca(int sum, int sr, int den)
+{
+	setlocale(0,"RUS");
+    next:
+    printf("Введите сумму вклада \n\n\n");
+    scanf("%d", &sum);
+    printf("Введите срок \n\n\n");
+    scanf("%d", &sr); 
+    if(sum<10000)
+    {
+    printf("Сумма вклада мала (Уровень бомж) \n\n\n");
+    goto next;    
+    }
+    if(sr>365)
+    {
+    printf("Слишком много дней \n\n\n");
+    goto next;    
+    }
+    if(sum<100000)
+    {
+    if(sr<31 && 0<=sr)
+    {  
+    printf("Ваша сумма составляет %d\n\n\n", vich(sum, sr, den));                       
+    }  
+    if(sr<121 && 31<=sr)
+    {  
+    printf("Ваша сумма составляет %d\n\n\n", vich(sum, sr, den));                       
+    }  
+    if(sr<241 && 121<=sr)
+    {  
+    printf("Ваша сумма составляет %d\n\n\n", vich(sum, sr, den));                       
+    }   
+    if(sr<366 && 241<=sr)
+    {  
+    printf("Ваша сумма составляет %d\n\n\n", vich(sum, sr, den));                       
+    }                    
+    }  
+    if(sum>100000)
+    {
+    if(sr<31 && 0<=sr)
+    {  
+    printf("Ваша сумма составляет %d\n\n\n", vich(sum, sr, den));                       
+    }  
+    if(sr<121 && 31<=sr)
+    {  
+    den=sum+sum/100*3;
+    printf("Ваша сумма составляет %d\n\n\n", vich(sum, sr, den));                       
+    }  
+    if(sr<241 && 121<=sr)
+    {  
+    printf("Ваша сумма составляет %d\n\n\n", vich(sum, sr, den));                       
+    }   
+    if(sr<366 && 241<=sr)
+    {  
+    printf("Ваша сумма составляет %d\n\n\n", vich(sum, sr, den));                       
+    } 
+    }    
+}
+}
+
 
 
 
